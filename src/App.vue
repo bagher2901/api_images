17adbd9aa1b6121ad5be29ad0348b1a25f024627
bagher2901 <bagher.m1377@gmail.com>
2021-10-06 14:03:27 +0330

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+
     </div>
     <router-view/>
   </div>
@@ -18,7 +17,7 @@
 }
 
 #nav {
-  padding: 30px;
+  /*padding: 30px;*/
 }
 
 #nav a {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,108 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <div class="box-gallery">
+            <div class="container-fluid ">
+                <div class="row mb-1">
+                    <div class="col-3 p-0 mt-1 mb-1 slider">
+                        <div class="row p-0">
+                            <img src="johnny-depp-wallpaper-13-1920x1080.jpg" alt="1" class="col-11">
+                            <img src="johnny-depp-wallpaper-26-1920x1080.jpg" alt="2" class="col-11">
+                            <img src="johnny-depp-wallpaper-27-1920x1080.jpg" alt="3" class="col-11">
+                            <img src="johnny-depp-wallpaper-45-1920x1080.jpg" alt="4" class="col-11">
+                            <img src="johnny-depp-wallpaper-5-1920x1080.jpg" alt="5" class="col-11">
+                        </div>
+
+
+                    </div>
+                    <div class="col-9 p-0 mt-1 mb-1 screen">
+
+                        <div class="show container">
+                            <img src="johnny-depp-wallpaper-6-1600x1200.jpg" class="john" alt="6">
+
+                        </div>
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+    // @ is an alias to /src
 
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+
+    export default {
+        name: 'Home',
+        components: {}
+    }
 </script>
+
+<style>
+
+      .box-gallery {
+        height: 100vh;
+    }
+
+      .box-gallery .container-fluid {
+        height: 100%;
+        overflow-x: hidden;
+    }
+
+      .box-gallery .container-fluid .row {
+        direction: rtl;
+        height: 100%;
+    }
+
+      .box-gallery .container-fluid .row .slider {
+        height: 100%;
+        overflow-y: scroll;
+        overflow-x: hidden;
+        direction: rtl;
+    }
+
+      .box-gallery .container-fluid .row .slider .row {
+        direction: ltr;
+    }
+
+      .box-gallery .container-fluid .row .screen {
+        height: 100%;
+    }
+
+      .box-gallery .container-fluid .row .screen .show {
+        height: 100%;
+        width: 100%;
+    }
+
+      .box-gallery .container-fluid .row .screen .show .john {
+        width: 90%;
+        height: 700px;
+        margin-right: 50px;
+        margin-top: 20px;
+    }
+
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    /* Track */
+    ::-webkit-scrollbar-track {
+        background: none;
+    }
+
+    /* Handle */
+    ::-webkit-scrollbar-thumb {
+        background: rgba(0, 0, 255, 0.685);
+    }
+
+    /* Handle on hover */
+    ::-webkit-scrollbar-thumb:hover {
+        background: rgba(0, 0, 255, 0.685);
+    }
+
+    /*# sourceMappingURL=style.css.map */
+
+</style>
